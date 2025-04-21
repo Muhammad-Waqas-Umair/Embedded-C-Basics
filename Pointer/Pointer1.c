18f7177d6e4a5ef7665fd39458491bfb9467656c
Muhammad Waqas Umair <waqasumair111@gmail.com>
2025-04-21 16:01:51 +0200

    update

--- Pointer/Pointer1.c
+++ Pointer/Pointer1.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
-// Global variable 
-long long int global_data = 0xFFFEABCD11112345;  
+long long int global_value = 0xFFFEABCD11112345;
 
 int main(void) 
 {
-  
-    char *byte_ptr = (char*)&global_data;
-    printf("Value at address %p (byte): 0x%x\n", byte_ptr, *byte_ptr);
+    char *byte_ptr = (char*)&global_value;
+    printf("Value at address %p (byte, size: %zu): 0x%x\n", byte_ptr, sizeof(char), *byte_ptr);
 
-    
-    int *int_ptr = (int*)&global_data;
-    printf("Value at address %p (int):   0x%x\n", int_ptr, *int_ptr);
+    int *int_ptr = (int*)&global_value;
+    printf("Value at address %p (int, size: %zu): 0x%x\n", int_ptr, sizeof(int), *int_ptr);
 
-    
-    short *short_ptr = (short*)&global_data;
-    printf("Value at address %p (short): 0x%x\n", short_ptr, *short_ptr);
+    short *short_ptr = (short*)&global_value;
+    printf("Value at address %p (short, size: %zu): 0x%x\n", short_ptr, sizeof(short), *short_ptr);
 
-
-    long long *ll_ptr = (long long*)&global_data;
-    printf("Value at address %p (long long): 0x%llx\n", ll_ptr, *ll_ptr);
+    long long *ll_ptr = (long long*)&global_value;
+    printf("Value at address %p (long long, size: %zu): 0x%llx\n", ll_ptr, sizeof(long long), *ll_ptr);
 
     return 0;
 }
